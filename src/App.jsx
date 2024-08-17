b5650d55bc3c58576cb295d29b431bad78c0ae40
NATESH T <113875267+TNATESH555@users.noreply.github.com>
2024-08-17 13:34:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Nav from './Nav';
-import BodyApp from './body';
+import BodyApp from './Body.jsx';
 
 import Footer from './Footer';
 
